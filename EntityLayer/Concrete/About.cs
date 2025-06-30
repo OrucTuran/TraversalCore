ced30f4a2fb2e748a1fa44806207780a99bdfedc
oructuran0 <orctrn193@gmail.com>
2025-06-30 09:12:50 +0300

    Hi Traversal Project!

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string ImageA { get; set; }
+        public string TitleA { get; set; }
+        public string DescriptionA { get; set; }
+        public string ImageB { get; set; }
+        public string TitleB { get; set; }
+        public string DescriptionB { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/BusinessRelation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class BusinessRelation//hoca about2 olarak aldi
+    {
+        public int BusinessRelationID { get; set; }
+        public string Image { get; set; }
+        public string TitleA { get; set; }
+        public string TitleB { get; set; }
+        public string Description { get; set; }
+    }
+}
