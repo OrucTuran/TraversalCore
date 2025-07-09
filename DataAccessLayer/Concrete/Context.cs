1480a20d5b053f19db32fa5abd1fac7585bd805c
oructuran0 <orctrn193@gmail.com>
2025-07-09 16:52:19 +0300

    Created Context class

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,14 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("Server=DESKTOP-1FB5EJT\\SQLEXPRESS;database=TraverselDb;integrated security=true;");
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<BusinessRelation> BusinessRelations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newslatter> Newslatters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
