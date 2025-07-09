e11469914ea1eba366543597e41c88018f08a56a
oructuran0 <orctrn193@gmail.com>
2025-07-09 16:55:03 +0300

    Primary Key assignments to context classes

--- EntityLayer/Concrete/BusinessRelation.cs
+++ EntityLayer/Concrete/BusinessRelation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class BusinessRelation//hoca about2 olarak aldi
     {
+        [Key]
         public int BusinessRelationID { get; set; }
         public string Image { get; set; }
         public string TitleA { get; set; }
