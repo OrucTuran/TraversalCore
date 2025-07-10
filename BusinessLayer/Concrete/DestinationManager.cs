042edc95d8241fd7817cf1ab648e951b55a96bfd
oructuran0 <orctrn193@gmail.com>
2025-07-10 21:51:37 +0300

    Listed popular destinations

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        private IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public Destination GeyByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Destination t)
+        {
+            _destinationDal.Insert(t);
+        }
+
+        public void TDelete(Destination t)
+        {
+            _destinationDal.Delete(t);
+        }
+
+        public List<Destination> TGetList()
+        {
+            return _destinationDal.GetList();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            _destinationDal.Update(t);
+        }
+    }
+}
