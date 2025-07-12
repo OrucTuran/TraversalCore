6cbfadff7ed383d91c37efafaf495b688905c632
oructuran0 <orctrn193@gmail.com>
2025-07-12 22:35:45 +0300

    SubAbout

---
+++ BusinessLayer/Concrete/SubAboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SubAboutManager : ISubAboutService
+    {
+        private ISubAboutDal _subAboutDal;
+
+        public SubAboutManager(ISubAboutDal subAboutDal)
+        {
+            _subAboutDal = subAboutDal;
+        }
+
+        public SubAbout GeyByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(SubAbout t)
+        {
+            _subAboutDal.Insert(t);
+        }
+
+        public void TDelete(SubAbout t)
+        {
+            _subAboutDal.Delete(t);
+        }
+
+        public List<SubAbout> TGetList()
+        {
+            return _subAboutDal.GetList();
+        }
+
+        public void TUpdate(SubAbout t)
+        {
+            _subAboutDal.Update(t);
+        }
+    }
+}
