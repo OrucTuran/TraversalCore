61b77bd9c89bf791f722569df114037862d82d18
oructuran0 <orctrn193@gmail.com>
2025-07-12 23:44:53 +0300

    Testimonials

---
+++ DataAccessLayer/Migrations/20250712204053_RecreateTestimonialsTable11.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class RecreateTestimonialsTable11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Testimonials",
+                columns: table => new
+                {
+                    TestimonialID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Client = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    ClientImage = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testimonials", x => x.TestimonialID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Testimonials");
+        }
+    }
+}
