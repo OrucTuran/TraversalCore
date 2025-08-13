09a27472145903d52af3b9aacb2e2f230b6623f2
oructuran0 <orctrn193@gmail.com>
2025-08-13 14:00:15 +0300

    Login With Identity

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -12,10 +12,12 @@ namespace TraversalCoreProject.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -59,10 +61,22 @@ namespace TraversalCoreProject.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult SignIn(int id)
-        //{
-        //	return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel userSignInViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var values = await _signInManager.PasswordSignInAsync(userSignInViewModel.Username, userSignInViewModel.Password, false, true);
+                if (values.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(SignIn));
+                }
+            }
+            return View();
+        }
     }
 }
---
+++ TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı girin.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi girin.")]
+        public string Password { get; set; }
+    }
+}
