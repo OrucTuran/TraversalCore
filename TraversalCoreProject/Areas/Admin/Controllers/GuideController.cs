301d4dce489b5c615de7bbd5b46d7f2922817d07
oructuran0 <orctrn193@gmail.com>
2025-09-10 12:01:37 +0300

    Add guide creation and editing functionality
    Introduced AddGuide and EditGuide actions in GuideController for creating and updating guides. Added corresponding views to support guide addition and editing in the admin area.

--- TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
@@ -24,10 +25,38 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
             var guide = _guideService.TGeyByID(id);
             if (guide != null)
             {
-                guide.Status = !guide.Status; // aktifse pasif yap, pasifse aktif
+                guide.Status = !guide.Status;
                 _guideService.TUpdate(guide);
             }
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var value = _guideService.TGeyByID(id);
+
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
